88d5f18b80930d7dca91a347f1ac50e6b9da81cf
Matt Jennings <mattjennings44@gmail.com>
2019-11-01 16:39:58 -0600

    fix dev dbName

--- dev.ts
+++ dev.ts
@@ -6,8 +6,7 @@ const server = new ApolloServer({
   context: ({ req }) => {
     return {
       dbUrl: process.env.DB_URL,
-      // dbName: req.headers.db,
-      dbName: 'yhufhcglgjveqzgn',
+      dbName: req.headers.db,
       onResolveConflict({ document, conflicts, context }) {
         return document
       },
